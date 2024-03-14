c797fb7a0f5a82dcc099fdbabf5f38ef6304c318
juliangubbins <162518670+juliangubbins@users.noreply.github.com>
2024-03-14 16:23:50 +0000

    memoria_dificil

--- juegos/memoria.py
+++ juegos/memoria.py
@@ -1,8 +1,26 @@
-def memoria():
-    """
-    Esta función representa el juego de memoria.
-    Debes generar una secuencia de números al azar y mostrarla al usuario.
-    Luego, debes pedir al usuario que repita la secuencia.
-    Se debe mostrar un mensaje si el usuario acierta o no.
-    """
-    pass
+def memoria(): 
+    """ 
+    Esta función representa el juego de memoria. 
+    Debes generar una secuencia de números al azar y mostrarla al usuario. 
+    Luego, debes pedir al usuario que repita la secuencia. 
+    Se debe mostrar un mensaje si el usuario acierta o no. 
+    """ 
+     
+    import random 
+    import time 
+ 
+    numero = random.randint(1000000, 9999999) 
+    print(f'Secuencia: {numero}') 
+ 
+    for i in range(5): 
+        time.sleep(1) 
+        print(5 - i) 
+ 
+    for i in range(20): 
+        print('XXXXXXXXXXXXXXXXXX') 
+ 
+    final = int(input('Introduce la secuencia: ')) 
+    if final == numero: 
+        print('Ganaste!') 
+    else: 
+        print('Perdiste :(')
